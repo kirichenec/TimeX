88956681c0a2a97453f4a920dddc7b02c5e18c46
kirichenec <kirichenecigor@gmail.com>
2019-03-25 00:01:49 +0300

    [+] Added blank project for v2 TimeX

---
+++ TimeX/TimeXv2/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace TimeXv2.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        public string Title { get; set; }
+
+        public MainViewModel()
+        {
+            Title = "Hello, World!!";
+        }
+    }
+}
