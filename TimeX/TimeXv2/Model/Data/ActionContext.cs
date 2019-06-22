c4f45f8fe5112f846e3670a4c11099637b5a91eb
kirichenec <kirichenecigor@gmail.com>
2019-06-22 14:54:29 +0300

    [~] Set preffered cond=structor for ActionContext

--- TimeX/TimeXv2/Model/Data/ActionContext.cs
+++ TimeX/TimeXv2/Model/Data/ActionContext.cs
@@ -1,10 +1,12 @@
-﻿using SQLite.CodeFirst;
+﻿using GalaSoft.MvvmLight.Ioc;
+using SQLite.CodeFirst;
 using System.Data.Entity;
 
 namespace TimeXv2.Model.Data
 {
     public class ActionContext : DbContext
     {
+        [PreferredConstructor]
         public ActionContext() : base($"{nameof(TimeXv2)}Context") { }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
